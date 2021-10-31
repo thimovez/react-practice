0f8d3aa3581fa7bc099d91f1959ea00b925d7fe5
Thimovez <thimovez.gmail.com>
2021-10-31 11:26:49 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
+//ядро реакта
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//Вызываем ф-цию рендер потом передаем компонент который необходимо отрисовать
+// В рут мы помещаем блок в который мы будем монтировать этот компонент --App
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
