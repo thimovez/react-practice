44461affefa3176af56d118b0262e0cea9f29eef
Thimovez <thimovez.gmail.com>
2021-11-01 12:47:30 +0200

    add blocks post on app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,13 +13,24 @@ function App() {
     {id: 2, title: 'Javascript', body: 'Description'},
     {id: 3, title: 'Javascript', body: 'Description'}
   ]);
+  const [title, setTitle] = useState('');
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Название поста"/>
-        <input type="text" placeholder="Описание поста"/>
-        <button>Создать пост</button>
+        {/*Управляемый компонент*/}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput type="text" placeholder="Описание поста"/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Посты про JS"/>
     </div>
