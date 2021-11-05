04dcf5d4b2b62e31d6cf588c4cb36d67c350f6ac
Thimovez <thimovez.gmail.com>
2021-11-05 20:44:19 +0200

    added new functionality to the application

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import About from "../pages/About";
+import Posts from "../pages/Posts";
+import Error from "../pages/Error";
+import PostIdPage from "../pages/PostIdPage";
+
+const AppRouter = () => {
+  return (
+    <Switch>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route exact path="/posts">
+        <Posts/>
+      </Route>
+      <Route exact path="/posts/:id">
+        <PostIdPage/>
+      </Route>
+      <Redirect to="/posts"/>
+    </Switch>
+  );
+};
+
+export default AppRouter;
